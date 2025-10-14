63f0451ff3678d6e775909405ba6445f8bfe8c68
Dennis Amtoft Jensen <dennis.jensen@specialisterne.dk>
2025-10-14 10:22:14 +0200

    project structure, test setup

---
+++ src/PDFDownloader/test_excel_reader.py
@@ -0,0 +1,14 @@
+# from excel_reader import ExcelReader
+
+# def main():
+#     reader = ExcelReader()
+
+#     try:
+#         data = reader.data("data/input/Test_ark.xlsx", "Pdf_URL", "BRnum")
+#         print("ExcelReader virker. Læste rækker:", len(data))
+#         print("Første 3:", data[:3])
+#     except Exception as e:
+#         print("Fejl i ExcelReader:", e)
+    
+# if __name__ == "__main__":
+#     main()
---
+++ tests/unittests/test_excel_reader.py
@@ -0,0 +1,71 @@
+import pytest
+from unittest.mock import patch
+
+import pandas as pd
+
+from PDFDownloader.excel_reader import ExcelReader
+from PDFDownloader.logger import LoggerService
+
+
+@pytest.fixture
+def mock_data_df():
+    return pd.DataFrame({"Pdf_URL": ["http://example.com"], "BRnum": ['123']}) 
+
+
+def test_read_data_valid_data(tmp_path, mock_data_df):
+    mock_data = [("http://example.com", '123')]
+    with patch("pandas.read_excel", return_value= mock_data_df):
+        reader = ExcelReader(LoggerService())
+        result = reader.read_data(tmp_path, "Pdf_URL", "BRnum")        
+        assert result == mock_data
+
+def test_read_data_invalid_excel_file(tmp_path, mock_data_df):
+    with patch("pandas.read_excel", 
+        side_effect= ValueError,
+        return_value= mock_data_df):
+        reader = ExcelReader(LoggerService())
+        with pytest.raises(ValueError):
+            reader.read_data(tmp_path, "Pdf_URL", "BRtum") 
+
+
+def test_read_data_invalid_column_url(tmp_path, mock_data_df):
+    mock_data = [("http://example.com", '123')]
+    with patch("pandas.read_excel", return_value= mock_data_df):
+        reader = ExcelReader(LoggerService())
+        with pytest.raises(ValueError):
+            reader.read_data(tmp_path, "Pdn_URL", "BRnum") 
+
+def test_read_data_invalid_column_name(tmp_path, mock_data_df):
+    mock_data = [("http://example.com", '123')]
+    with patch("pandas.read_excel", return_value= mock_data_df):
+        reader = ExcelReader(LoggerService())
+        with pytest.raises(ValueError):
+            reader.read_data(tmp_path, "Pdf_URL", "BRtum") 
+
+def test_read_data_file_not_found(tmp_path):
+    with patch("pandas.read_excel", side_effect=FileNotFoundError()):
+        reader = ExcelReader(LoggerService())
+        with pytest.raises(FileNotFoundError):
+            reader.read_data(f"{tmp_path}/missing.xlsx", "Pdf_URL", "BRnum")
+
+def test_read_data_invalid_url(tmp_path):    
+    with patch("pandas.read_excel", return_value=
+        pd.DataFrame({"Pdf_URL": ["invalidurl"], "BRnum": ['123']} )):
+        reader = ExcelReader(LoggerService())
+        with pytest.raises(ValueError, match="Ingen gyldige URL'er fundet i filen."):
+            reader.read_data(tmp_path, "Pdf_URL", "BRnum") 
+
+def test_read_data_empty_name(tmp_path):    
+    with patch("pandas.read_excel", return_value=
+        pd.DataFrame({"Pdf_URL": ["http://example.com"], "BRnum": ['']} )):
+        reader = ExcelReader(LoggerService())
+        with pytest.raises(ValueError):
+            reader.read_data(tmp_path, "Pdf_URL", "BRnum") 
+
+def test_read_data_empty_url(tmp_path):    
+    with patch("pandas.read_excel", return_value=
+        pd.DataFrame({"Pdf_URL": [""], "BRnum": ['123']} )):
+        reader = ExcelReader(LoggerService())
+        with pytest.raises(ValueError, match="Ingen gyldige URL'er fundet i filen."):
+            reader.read_data(tmp_path, "Pdf_URL", "BRnum") 
+
